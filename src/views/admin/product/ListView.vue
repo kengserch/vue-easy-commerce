e0b0c3938e7b1d6060cfd4a03c0ac0c491772c52
Prasert Saengkaew <kengsert@gmail.com>
2024-06-18 16:57:35 +0700

    crud-admin-product

--- src/views/admin/product/ListView.vue
+++ src/views/admin/product/ListView.vue
@@ -1,9 +1,78 @@
 <script setup>
+//config
+import { RouterLink } from "vue-router";
+import { onMounted } from "vue";
+//layout
 import AdminLayout from "@/layouts/AdminLayout.vue";
+//component
+import Edit from "@/components/icons/Edit.vue";
+import Trash from "@/components/icons/Trash.vue";
+//store
+import { useAdminProductStore } from "@/stores/admin/product";
+
+const adminProductStore = useAdminProductStore();
+
+onMounted(() => {
+    adminProductStore.loadProduct();
+});
+
+const removeProduct = (index) => {
+    adminProductStore.removeProduct(index)
+}
 </script>
 
 <template>
-    <AdminLayout> 
-        Product List View 
+    <AdminLayout>
+        <div class="flex items-center justify-between my-4">
+            <div class="text-3xl font-bold">Product</div>
+            <div>
+                <RouterLink :to="{ name: 'admin-products-create' }" class="btn btn-neutral">Add New</RouterLink>
+            </div>
+        </div>
+        <div class="overflow-x-auto">
+            <table class="table">
+                <!-- head -->
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Image</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Status</th>
+                        <th>Updated At</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- row 1 -->
+                    <tr v-for="(product,index) in adminProductStore.list" :key="index">
+                        <th>{{ product.name }}</th>
+                        <td>
+                            <img :src="product.image" class="w-12" />
+                        </td>
+                        <td>{{ product.price }}</td>
+                        <td>{{ product.remainQuantity }} / {{ product.quantity }}</td>
+                        <td>
+                            <div 
+                                class="badge" 
+                                :class="product.status === 'open'? 'badge-success': 'badge-error' ">
+                                {{ product.status }}
+                            </div>
+                        </td>
+                        <td>{{ product.updatedAt }}</td>
+                        <td>
+                            <div class="flex">
+                                <RouterLink :to="{name: 'admin-products-update' , params : {id : index}}" class="btn btn-ghost">
+                                    <Edit class="w-5"></Edit>
+                                </RouterLink>
+                                <button @click="removeProduct(index)" class="btn btn-ghost">
+                                    <Trash class="w-5"></Trash>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </AdminLayout>
 </template>
--- src/views/admin/product/UpdateView.vue
+++ src/views/admin/product/UpdateView.vue
@@ -1,9 +1,107 @@
 <script setup>
+//config
+import { reactive, onMounted, ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+//layout
 import AdminLayout from "@/layouts/AdminLayout.vue";
+//store
+import { useAdminProductStore } from "@/stores/admin/product";
+
+const formData = [
+    {
+        name: "Name",
+        field: "name",
+    },
+    {
+        name: "Image",
+        field: "image",
+    },
+    {
+        name: "Price",
+        field: "price",
+    },
+    {
+        name: "Quantity",
+        field: "quantity",
+    },
+    {
+        name: "About",
+        field: "about",
+    },
+];
+
+const adminProductStore = useAdminProductStore();
+const router = useRouter();
+const route = useRoute();
+
+const productIndex = ref(-1);
+const mode = ref("ADD");
+
+const productData = reactive({
+    name: "",
+    image: "",
+    price: 0,
+    quantity: 0,
+    about: "",
+    status: "",
+});
+const updateProduct = () => {
+    if (mode.value === "EDIT") {
+        adminProductStore.updateProduct(productIndex.value, productData)
+    } else {
+        //console.log(productData)
+        adminProductStore.addProduct(productData);
+    }
+    router.push({ name: "admin-products-list" });
+};
+
+onMounted(() => {
+    if (route.params.id) {
+        mode.value = "EDIT";
+
+        productIndex.value = parseInt(route.params.id);
+        const selectedProduct = adminProductStore.getProduct(productIndex.value);
+        // console.log('selectedProduct',selectedProduct)
+        productData.name = selectedProduct.name;
+        productData.image = selectedProduct.image;
+        productData.price = selectedProduct.price;
+        productData.quantity = selectedProduct.quantity;
+        productData.about = selectedProduct.about;
+        productData.status = selectedProduct.status;
+    }
+});
 </script>
 
 <template>
-    <AdminLayout> 
-        Product Update View 
+    <AdminLayout>
+        <div class="shadow-xl p-8 mt-4">
+            <div class="text-1xl font-bold">{{ mode }}</div>
+            <div class="divider"></div>
+            <div class="grid grid-cols-2 gap-5">
+                <label v-for="form in formData" :key="form.name" class="form-control w-full">
+                    <div class="label">
+                        <span class="label-text">{{ form.name }}</span>
+                    </div>
+                    <input type="text" :placeholder="form.field" class="input input-bordered w-full" v-model="productData[form.field]" />
+                </label>
+            </div>
+            <div class="divider"></div>
+            <div class="grid grid-cols-2 gap-5">
+                <label class="form-control w-full">
+                    <div class="label">
+                        <span class="label-text">Status</span>
+                    </div>
+                    <select v-model="productData.status" class="select select-bordered">
+                        <option disabled selected>Choose status</option>
+                        <option value="open">Open</option>
+                        <option value="close">Close</option>
+                    </select>
+                </label>
+            </div>
+            <div class="mt-4 flex justify-end">
+                <button class="btn btn-ghost">Back</button>
+                <button class="btn btn-neutral" @click="updateProduct">{{ mode }}</button>
+            </div>
+        </div>
     </AdminLayout>
 </template>
