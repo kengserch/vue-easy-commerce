b0ed5c947edbde45555c42dc9c22eaffe57413f0
Prasert Saengkaew <kengsert@gmail.com>
2024-06-19 15:00:45 +0700

    crud-admin-user

--- src/views/admin/user/ListView.vue
+++ src/views/admin/user/ListView.vue
@@ -1,9 +1,50 @@
 <script setup>
+//config
+import { RouterLink } from "vue-router";
+//layout
 import AdminLayout from "@/layouts/AdminLayout.vue";
+//component
+import Edit from "@/components/icons/Edit.vue";
+import Trash from "@/components/icons/Trash.vue";
+import Table from "@/components/Table.vue";
+
+//store
+import { useAdminUserStore } from "@/stores/admin/user";
+
+const adminUserStore = useAdminUserStore()
+
+const changeStatus = (index) => {
+    let selectedUser = adminUserStore.list[index]
+    selectedUser.status = selectedUser.status === 'active' ? 'inactive' : 'active'
+    adminUserStore.updateUser(selectedUser)
+}
+
 </script>
 
 <template>
     <AdminLayout> 
-        User List View 
+        <div class="flex items-center justify-between my-4">
+            <div class="text-3xl font-bold">User</div>
+        </div>
+        <Table :headers="['Name', 'Role', 'Status', 'UpdatedAt', '' ]">
+            <tr v-for="(user,index) in adminUserStore.list" :key="index">
+                <td>{{ user.fullname }}</td>
+                <td>{{ user.role }}</td>
+                <td>
+                    <div class="badge" :class="user.status === 'active' ? 'badge-success' : 'badge-error'">
+                        {{ user.status }}
+                    </div>
+                </td>
+                <td>{{ user.updatedAt }}</td>
+                <td>
+                    <div class="flex gap-2">
+                        <RouterLink :to="{ name : 'admin-users-update', params : {id : index }}" class="btn">Edit</RouterLink>
+                        <button class="btn" @click="changeStatus(index)">
+                           {{user.status === 'active' ? 'Disable' : 'Enable'}}
+                        </button>
+                    </div>
+                </td>
+            </tr>
+        </Table>
     </AdminLayout>
 </template>
--- src/views/admin/user/UpdateView.vue
+++ src/views/admin/user/UpdateView.vue
@@ -1,9 +1,81 @@
 <script setup>
+//config
+import { ref, reactive, onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+//layout
 import AdminLayout from "@/layouts/AdminLayout.vue";
+//store
+import { useAdminUserStore } from "@/stores/admin/user";
+
+const formData = [
+    {
+        name: "Name",
+        field: "fullname",
+        type: "text",
+    },
+    {
+        name: "Role",
+        field: "role",
+        type: "select",
+        dropdownList: ["admin", "moderator", "user"],
+    },
+    {
+        name: "Status",
+        field: "status",
+        type: "select",
+        dropdownList: ["active", "inactive"],
+    },
+];
+
+const adminUserStore = useAdminUserStore();
+const route = useRoute();
+const router = useRouter()
+
+const userIndex = ref(-1);
+const userData = reactive({
+    fullname: "",
+    role: "",
+    status: "",
+});
+
+onMounted(() => {
+    if (route.params.id) {
+        userIndex.value = parseInt(route.params.id);
+        const selectedUser = adminUserStore.getUser(userIndex.value);
+        userData.fullname = selectedUser.fullname;
+        userData.role = selectedUser.role;
+        userData.status = selectedUser.status;
+    }
+});
+
+const updateUser = () => {
+    adminUserStore.updateUser(userIndex.value,userData)
+    router.push({name:'admin-users-list'})
+};
 </script>
 
 <template>
-    <AdminLayout> 
-        User Update View 
+    <AdminLayout>
+        <div class="shadow-xl p-8 mt-4">
+            <div class="text-1xl font-bold">Update User :</div>
+            <div class="divider"></div>
+            <div class="grid grid-cols-1 gap-5">
+                <label v-for="form in formData" :key="form.name" class="form-control w-full">
+                    <div class="label">
+                        <span class="label-text">{{ form.name }}</span>
+                    </div>
+                    <input v-if="form.type === 'text'" v-model="userData[form.field]" type="text" :placeholder="form.field" class="input input-bordered w-full" />
+                    <select v-if="form.type === 'select'" v-model="userData[form.field]" type="text" :placeholder="form.field" class="input input-bordered w-full">
+                        <option v-for="(item, index) in form.dropdownList" :key="index">
+                            {{ item }}
+                        </option>
+                    </select>
+                </label>
+            </div>
+            <div class="mt-4 flex justify-end">
+                <button class="btn btn-ghost">Back</button>
+                <button class="btn btn-neutral" @click="updateUser">Update</button>
+            </div>
+        </div>
     </AdminLayout>
 </template>
